dfc36fb5be8fd203994abd4085c874d51af4959f
rehaangidwani <r3208795@gmail.com>
2025-06-30 14:34:17 +0200

    variables

--- Lesson 2 ACP/BIrthdays.py
+++ Lesson 2 ACP/BIrthdays.py
@@ -1,5 +1,10 @@
-print("I am" ,14, "years old")
-print("My mom is" ,37,"years old")
-print("My dad is" ,46,"years old")
-print("My grandmother is",80,"years old")
-print("My grandfather is",81,"years old")
+myage=14
+momage=37
+dadage=46
+grandmotherage=80
+grandfather=81
+print("I am" ,myage, "years old")
+print("My mom is" ,momage,"years old")
+print("My dad is" ,dadage,"years old")
+print("My grandmother is",grandmotherage,"years old")
+print("My grandfather is",grandfather,"years old")
