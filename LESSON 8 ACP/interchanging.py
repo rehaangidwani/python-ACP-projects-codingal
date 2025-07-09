da5038fd400ae4308835868783ea082fb47dc027
rehaangidwani <r3208795@gmail.com>
2025-07-09 22:56:47 +0200

    LESSON 8 ACP

---
+++ LESSON 8 ACP/interchanging.py
@@ -0,0 +1,9 @@
+
+a = input("Enter value for a: ")
+b = input("Enter value for b: ")
+c = input("Enter value for c: ")
+a, b, c = c, a, b
+print("After swapping:")
+print("a =",a )
+print("b =",a )
+print("c =",a)
