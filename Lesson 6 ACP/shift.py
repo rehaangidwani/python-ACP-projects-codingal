ea24473cf83cf6d038544ccce3d693f9849f9962
rehaangidwani <r3208795@gmail.com>
2025-07-09 15:21:18 +0200

    LESSON 7 PART 3 ACP

---
+++ Lesson 6 ACP/shift.py
@@ -0,0 +1,6 @@
+x=21
+y=20
+print("x left shift by one place=",x<<1)
+print("y left shift by three places=",x<<3)
+print("x right shift by one place=",x>>1)
+print("y right shift by three places=",x>>3)
