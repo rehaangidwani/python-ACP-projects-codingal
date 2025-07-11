218978ba07b89938af06a9ce2b6016f5e709b962
rehaangidwani <r3208795@gmail.com>
2025-07-11 13:14:51 +0200

    Lesson 9 acp

---
+++ Lesson 9 ACP/age.py
@@ -0,0 +1,6 @@
+age = int(input("Enter the student's age: "))
+
+if age >= 10 and age <= 20:
+    print("Student is allowed to join the class.")
+else:
+    print("Student is not allowed to join the class.")
