a44f53e0be033f61b0bee4547c39b21e59394d03
rehaangidwani <r3208795@gmail.com>
2025-07-14 12:37:06 +0200

    LESSON 10 ACP

--- Lesson 9 ACP/age.py
+++ Lesson 9 ACP/age.py
@@ -3,4 +3,8 @@ age = int(input("Enter the student's age: "))
 if age >= 10 and age <= 20:
     print("Student is allowed to join the class.")
 else:
+    if age %2==0:
+        print("its an even number")
+    else:
+        print("youre age is an odd number")
     print("Student is not allowed to join the class.")
