212aa807657829518e88403980698da7ce2573f3
rehaangidwani <r3208795@gmail.com>
2025-07-24 12:52:29 +0200

    LESSON 16 ACP

---
+++ LESSON 16 ACP/shutdown.py
@@ -0,0 +1,7 @@
+def shutdown(user_input):
+    if user_input == "Yes":
+        print("shutting down")
+    elif user_input == "no":
+        print("abort shut down")
+    else:
+        print("sorry")
