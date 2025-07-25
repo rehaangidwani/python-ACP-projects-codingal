405675ac7a34d00336735d4c10122420df32a374
rehaangidwani <r3208795@gmail.com>
2025-07-25 18:00:08 +0200

    Lesson 17 ACP

---
+++ Lesson 17 acp/Bill.py
@@ -0,0 +1,12 @@
+def calculate_change():
+    total_bill = 2.50
+    amount_paid = 4.00
+
+    change = amount_paid - total_bill
+    change = amount_paid - total_bill
+    print(f"The shopkeeper should return ${change:.2f} to Vishal.")
+
+calculate_change()
+
+
+
